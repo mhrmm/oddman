6e65e1f1cba2ff59844e1efef92361fd7d68d229
hopkinsm <hopkinsm@C02X456YJHD3.local>
2018-11-19 14:57:35 -0800

    Added a tied omo network.

--- puzzle.py
+++ puzzle.py
@@ -11,11 +11,20 @@ def oneHot(word, vocab):
     return vec
 
 def makePuzzleVector(puzzle, vocab):
-    (num1, num2, num3), _ = puzzle
-    oneHot1 = oneHot(str(num1), vocab)
-    oneHot2 = oneHot(str(num2), vocab)
-    oneHot3 = oneHot(str(num3), vocab)
-    return torch.FloatTensor(oneHot1 + oneHot2 + oneHot3).view(1, -1)
+    choices, _ = puzzle
+    oneHotVec = []
+    for choice in choices:
+        oneHotVec += oneHot(str(choice), vocab)
+    #oneHot2 = oneHot(str(num2), vocab)
+    #oneHot3 = oneHot(str(num3), vocab)
+    return torch.FloatTensor(oneHotVec).view(1, -1)
+
+def makePuzzleVectorAlt(puzzle, vocab):
+    choices, _ = puzzle
+    oneHotVec = []
+    for choice in choices:
+        oneHotVec += oneHot(str(choice), vocab)
+    return torch.FloatTensor(oneHotVec).view(len(choices), -1)
 
 
 def makePuzzleTarget(label):
@@ -33,7 +42,7 @@ def flipCoin():
     return random.random() < 0.5
     
 
-class OddManOutPuzzleGenerator:
+class PuzzleGenerator:
     def __init__(self):
         pass
     
@@ -41,10 +50,10 @@ class OddManOutPuzzleGenerator:
         return [self.generate() for n in range(N)]
 
 
-class TwoDigitPuzzleGenerator(OddManOutPuzzleGenerator):
+class TwoDigitPuzzleGenerator(PuzzleGenerator):
     
     def __init__(self, digits):
-        super(TwoDigitPuzzleGenerator, self).__init__()
+        super(PuzzleGenerator, self).__init__()
         self.digits = digits
 
     def makeTwoDigitNumber(self, digit1, digit2):
@@ -71,3 +80,67 @@ class InvertingTwoDigitPuzzleGenerator(TwoDigitPuzzleGenerator):
         if flipCoin():
             num = digit2 * 10 + digit1
         return num
+    
+class OddManOutPuzzleGenerator(PuzzleGenerator):
+    
+    def __init__(self, buckets, choicesPerPuzzle):
+        self.buckets = dict()
+        # Remove any buckets that don't contains enough choices
+        for bucket in buckets:
+            if len(buckets[bucket]) >= choicesPerPuzzle - 1:
+               self.buckets[bucket] = buckets[bucket] 
+        if len(self.buckets) < 2:
+            raise Exception("""Argument 'buckets' must have 
+                            at least 2 keys: {}""".format(buckets))
+        self.choicesPerPuzzle = choicesPerPuzzle
+ 
+    def getCandidatePuzzle(self):
+        categories = list(self.buckets.keys())
+        bucket = random.choice(categories)
+        otherBucket = random.choice(categories)            
+        correct = random.sample(self.buckets[bucket], self.choicesPerPuzzle - 1)
+        oddman = random.choice(self.buckets[otherBucket])
+        return (correct, oddman)
+
+    def countCommonCategories(self, choices):
+        return len([bucket for bucket in self.buckets 
+                    if choices.issubset(set(self.buckets[bucket]))])
+        
+
+    def findOddmen(self, choices):
+        choiceset = set(choices)
+        num_common = self.countCommonCategories(choiceset)
+        oddmen = []
+        for i, oddman in enumerate(choices):
+            remainder = choiceset - set([oddman])
+            if self.countCommonCategories(remainder) > num_common:
+                oddmen.append(i)
+        return oddmen
+            
+    
+    def generate(self):
+        (correct, oddman) = self.getCandidatePuzzle()
+        while len(self.findOddmen(correct + [oddman])) != 1:
+            (correct, oddman) = self.getCandidatePuzzle()            
+        choices = correct + [oddman]
+        random.shuffle(choices)
+        answer = self.findOddmen(choices)[0]
+        return (choices, answer)
+    
+class AltTwoDigitPuzzleGenerator(PuzzleGenerator):
+    
+    def __init__(self, digits, choicesPerPuzzle):
+        buckets = dict()
+        for digit1 in digits:
+            bucket = set()
+            for digit2 in digits:
+                bucket.add(str(digit1) + str(digit2))
+                bucket.add(str(digit2) + str(digit1))
+            buckets[str(digit1)] = list(bucket)
+        self.generator = OddManOutPuzzleGenerator(buckets, choicesPerPuzzle)
+        
+    def generate(self):
+        return self.generator.generate()
+            
+        
+        
